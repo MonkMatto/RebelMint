8c1096d6ec8dcd928bd0ae08c31c2ee904aea80a
tophercully <89924670+tophercully@users.noreply.github.com>
2024-05-30 17:07:49 +0200

    Wallet connection set up

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,40 @@
-import React, { useState } from "react";
+import { createWeb3Modal, useWeb3Modal } from "@web3modal/wagmi/react";
+import React, { useEffect, useState } from "react";
+import { useWalletInfo } from "@web3modal/wagmi/react";
+import { useAccount } from "wagmi";
 
 interface OpenMintProps {
   buttonText: string;
 }
 
 const OpenMintButton = ({ buttonText }: OpenMintProps) => {
+  const { open } = useWeb3Modal();
+  const { walletInfo } = useWalletInfo();
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const [userAddress, setUserAddress] = useState(null);
+  const shortAddress = address
+    ? address.substring(0, 4) +
+      "..." +
+      address.substring(address.length - 4, address.length)
+    : null;
+
+  console.log(address ? address : "no wallet connected");
+  const handleConnect = () => {
+    isDisconnected ? open({ view: "Networks" }) : open();
+    setUserAddress(address);
+  };
+
+  useEffect(() => {
+    console.log(userAddress);
+  }, [userAddress]);
+
   return (
-    <div className="w-full h-full text-xl">
-      {buttonText ? buttonText : "MINT"}
-    </div>
+    <button
+      onClick={handleConnect}
+      className="w-fit h-fit bg-white rounded-md text-md"
+    >
+      {address ? shortAddress : buttonText}
+    </button>
   );
 };
 
